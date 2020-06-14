7eb69a8e5d36ccd32f699a485f95c1bd5d9f26b6
Vu Duc Thuan <vuducthuan1994@gmail.com>
2020-06-14 23:55:10 +0700

    fix ok

--- routes/client.js
+++ routes/client.js
@@ -3,8 +3,8 @@ let router = express.Router();
 
 
 const homeController = require('./client/home.js');
-
+const aboutUsController = require('./client/about-us.js');
 router.use('/', homeController);
-
+router.use('/about-us', aboutUsController);
 
 module.exports = router;
--- routes/client/about-us.js
+++ routes/client/about-us.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('client/index', {
+    res.render('client/about-us', {
         title: "Sửa Thông Tin Kho Hàng",
         layout: 'client.hbs',
     });
