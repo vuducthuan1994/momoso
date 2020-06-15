282c20137bf16f9e95ffcb2dcd6832baae7280a4
Vũ Đức Thuận <vdthuan@vnpt.vn>
2020-06-15 14:21:05 +0700

    fix ok

---
+++ clientTheme/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    console.log("ready!");
+    showToastSubscribe();
+});
+
+function showToastSubscribe() {
+    // https://github.com/kamranahmedse/jquery-toast-plugin
+    $.toast({
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, consequuntur doloremque eveniet eius eaque dicta repudiandae illo ullam. Minima itaque sint magnam dolorum asperiores repudiandae dignissimos expedita, voluptatum vitae velit.",
+        hideAfter: false
+    })
+}
