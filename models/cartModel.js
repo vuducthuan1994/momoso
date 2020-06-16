add87fa98c8e27a905519fb7b5bc9f8a68a99a91
Vũ Đức Thuận <vdthuan@vnpt.vn>
2020-06-16 15:37:14 +0700

    fix ok

---
+++ models/cartModel.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var CartSchema = new Schema({
+    seasonID: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    listCartProducts: {
+        type: Array,
+        default: []
+    },
+    listFavorProducts: {
+        type: Array,
+        default: []
+    },
+    listTotals: {
+        type: Array,
+        default: []
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    },
+    updated_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+let Cart = mongoose.model('Carts', CartSchema);
+module.exports = Cart;
