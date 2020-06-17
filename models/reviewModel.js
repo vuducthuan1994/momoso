ebe0df18eac90bd723f18753f5ec18a4bb984351
Vũ Đức Thuận <vdthuan@vnpt.vn>
2020-06-17 10:51:33 +0700

    fix ok

---
+++ models/reviewModel.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var ReviewSchema = new Schema({
+    sessionID: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    rate: {
+        type: Number,
+        trim: true,
+        required: true
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    },
+    updated_date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+let Review = mongoose.model('Reviews', ReviewSchema);
+module.exports = Storage;
