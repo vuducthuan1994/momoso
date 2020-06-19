ec363cee85b9c0d92a885fcb3f5f89b5da34abf7
Vu Duc Thuan <vuducthuan1994@gmail.com>
2020-06-19 23:58:46 +0700

    fix ok

--- routes/admin/category.js
+++ routes/admin/category.js
@@ -6,7 +6,7 @@ var path = require('path');
 const fs = require('fs');
 const formidable = require('formidable');
 var uslug = require('uslug');
-
+const Product = require('../../models/productModel');
 var isAuthenticated = function(req, res, next) {
     if (process.env.ENV == 'DEV') {
         return next();
@@ -42,17 +42,17 @@ router.get('/add-category', isAuthenticated, function(req, res) {
 
 router.get('/edit-category/:id', isAuthenticated, function(req, res) {
     const categoryID = req.params.id;
-    Category.findOne({ _id: categoryID }, function(err, storage) {
+    Category.findOne({ _id: categoryID }, function(err, category) {
         if (err) {
             req.flash('messages', 'Lỗi hệ thống, không sửa được Loại SP !')
             res.redirect('back');
         } else {
-            res.render('admin/pages/storage/add-storage', {
+            res.render('admin/pages/category/add-category', {
                 errors: req.flash('errors'),
                 messages: req.flash('messages'),
                 title: "Sửa Thông Tin Kho Hàng",
                 layout: 'admin.hbs',
-                storage: storage.toJSON()
+                category: category ? category.toJSON() : null
             });
         }
     })
@@ -113,19 +113,80 @@ router.post('/', async function(req, res) {
 
 //edit category
 router.post('/edit-category/:id', function(req, res) {
+
+    let resizeWidth = 370;
+    let content = {};
     const idBanner = req.params.id;
     req.body.updated_date = new Date();
-    Category.updateOne({ _id: idBanner }, req.body, function(err, data) {
-        if (!err) {
-            req.flash('messages', 'Sửa kho hàng thành công !');
-            res.redirect('back');
-        } else {
-            req.flash('errors', 'Không sửa được Kho hàng');
-            res.redirect('back');
+    const form = formidable({ multiples: true });
+    form.parse(req);
+
+    form.on('field', function(fieldName, fieldValue) {
+        content[fieldName] = fieldValue;
+        if (fieldName == 'typeImage') {
+            if (fieldValue !== 'small') {
+                resizeWidth = 700;
+            }
+        }
+
+
+    });
+
+    form.on('file', function(fieldName, file) {
+        if (fieldName == 'imageUrl' && file.name !== '') {
+            const imgName = uslug((new Date().getTime() + '-' + file.name), { allowedChars: '.', lower: true });
+            var dir = __basedir + '/public/img/category';
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, 0744);
+            }
+            const img_path = path.join(__basedir, `public/img/category/${imgName}`);
+            content['imageUrl'] = `/img/category/${imgName}`;
+            resizeImages(resizeWidth, file.path, img_path);
         }
     });
-});
 
+    form.on('end', function() {
+
+        Category.findOneAndUpdate({ _id: idBanner }, content, { new: true }, function(err, category) {
+            updateCategoryInProduct(category);
+            if (!err) {
+                req.flash('messages', 'Sửa kho hàng thành công !');
+                res.redirect('back');
+            } else {
+                req.flash('errors', 'Không sửa được Kho hàng');
+                res.redirect('back');
+            }
+        });
+
+    });
+
+});
+let updateCategoryInProduct = function(category) {
+    let id = category._id.toString();
+    Product.findOneAndUpdate({ "category._id": id }, {
+            $set: {
+                "category.$.name": category.name,
+                "category.$.urlSeo": category.urlSeo,
+                "category.$.imageUrl": category.imageUrl,
+                "category.$.text": category.name,
+                "category.$.isShow": category.isShow,
+                "category.$.typeImage": category.typeImage
+
+            }
+        }, { new: true },
+
+        function(err, data) {
+            if (err) {
+                console.log(err);
+
+            } else {
+                // console.log(data);
+                console.log("update thành công")
+            }
+        }
+
+    );
+}
 router.get('/delete/:id', isAuthenticated, function(req, res) {
     const id = req.params.id;
     const messages = [];
